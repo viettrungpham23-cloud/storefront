5ab165077bde70b356b05b6b2f5a11af36a0ede2
VETC <vetc@MacBook-Pro.local>
2026-06-26 17:27:19 +0700

    fix(android): register GoogleAuth plugin in MainActivity to prevent crash

--- mobile/android/app/src/main/java/vn/tainnovation/store/MainActivity.java
+++ mobile/android/app/src/main/java/vn/tainnovation/store/MainActivity.java
@@ -1,5 +1,13 @@
 package vn.tainnovation.store;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.registerPlugin(GoogleAuth.class);
+    }
+}
